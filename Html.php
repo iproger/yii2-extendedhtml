04efc99f9c2e6d35bedc8eb6c4ce5131a0874d83
Mykhailo <proger.mixa@gmail.com>
2016-05-07 14:39:41 -0400

    main file

---
+++ Html.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace iproger\extendedhtml;
+
+use yii\helpers\Html;
+
+class Html extends Html
+{
+
+    public static function dataList($name, $items = [], $options = [])
+    {
+        static::addVoidElement('option');
+
+        if (!empty($options['multiple'])) {
+            return static::listBox($name, null, $items, $options);
+        }
+        $options['name'] = $name;
+        unset($options['unselect']);
+        $selectOptions = static::renderSelectOptions(null, $items, $options);
+        $result = static::tag('datalist', "\n" . $selectOptions . "\n", $options);
+
+        static::removeVoidElement('option');
+
+        return $result;
+    }
+
+    public static function addVoidElement($name)
+    {
+        static::$voidElements[$name] = 1;
+    }
+
+    public static function removeVoidElement($name)
+    {
+        if (in_array($name, static::$voidElements)) {
+            unset(static::$voidElements[$name]);
+        }
+    }
+
+}
